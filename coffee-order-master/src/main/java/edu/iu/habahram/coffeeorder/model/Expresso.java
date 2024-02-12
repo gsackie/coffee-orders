f2ac7a1df65d15762d7cf06d875fd9d428ed1cef
gsack <gsackie@iu.edu>
2024-02-12 08:57:38 -0500

    In class work

---
+++ coffee-order-master/src/main/java/edu/iu/habahram/coffeeorder/model/Expresso.java
@@ -0,0 +1,14 @@
+package edu.iu.habahram.coffeeorder.model;
+
+public class Expresso extends Beverage{
+
+        @Override
+        public String getDescription() {
+            return "Expresso";
+        }
+        @Override
+        public float cost() {
+            return 1.99F;
+        }
+
+}
